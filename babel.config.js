57ee0a1cc79ac6d7fcc9e07d4e48e2c2e1ba17cb
xlz122 <122874722@qq.com>
2024-10-18 10:38:07 +0800

    chore: 更新react native 0.74.6

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
     [
       'module-resolver',
       {
         root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        extensions: ['.android.js', '.ios.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@': ['./src']
         }
